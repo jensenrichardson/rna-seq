2621bf460bacdc0c329e9cc2e74da1b124543dfc
jensenrichardson <jensen.richardson@utexas.edu>
2021-03-03 13:19:49 -0600

    Updates to git ignore and finalized parse_samples.py

--- snakemake/parse_samples.py
+++ snakemake/parse_samples.py
@@ -2,8 +2,6 @@
 # This script is responsible for parsing a directory of
 # fastq samples to produce a yaml that is easy to manage
 # with snakemake.
-import numpy as np
-import pandas as pd
 from os import listdir
 from os.path import isfile, join
 import os
@@ -13,7 +11,6 @@ import re
 # for yaml
 import yaml
 # for arguments
-import sys
 import argparse
 
 import glob
@@ -21,15 +18,11 @@ import glob
 
 # Defines class Sample with name string and readgroups list
 class Sample:
-    def __init__(self):
-        self.name = ""
-        self.readgroups = []
-        self.command = ""
-    def __init__(self, name):
+    def __init__(self, name=""):
         self.name = name
         self.readgroups = []
         self.command = ""
-    
+
     def __repr__(self):
         rep = f'Sample(name: {self.name}, readgroups: {self.readgroups}, command: {self.command})'
         return rep
@@ -37,26 +30,29 @@ class Sample:
     def addReadgroup(self, rg):
         self.readgroups.append(rg) if rg not in self.readgroups else self.readgroups
 
+
 class Readgroup:
     # name of readgroup
-    def __init__(self, rg = ""):
+    def __init__(self, rg=""):
         self.rg = rg
         self._r1 = ""
         self._r2 = ""
 
-    #Equality
+    # Equality
     def _is_valid_operand(self, other):
-        return (hasattr(other, "rg"))
+        return hasattr(other, "rg")
+
     def __eq__(self, other):
         if not self._is_valid_operand(other):
             return NotImplemented
-        return (self.rg == other.rg)
-    #better printing
+        return self.rg == other.rg
+
+    # better printing
     def __repr__(self):
         rep = f'Readgroup(rg: {self.rg}, r1: {self.r1}, r2: {self.r2})'
         return rep
 
-    #getters and setters for r1 and r2 that ensure that the file exists
+    # getters and setters for r1 and r2 that ensure that the file exists
     @property
     def r1(self):
         return self._r1
@@ -77,6 +73,7 @@ class Readgroup:
             raise OSError(f'{r2} is not a file')
         self._r2 = r2
 
+
 # Used for ensuring directory provided as argument is a valid directory
 def dir_path(string):
     if os.path.isdir(string):
@@ -84,11 +81,12 @@ def dir_path(string):
     else:
         raise NotADirectoryError(string)
 
+
 def main(args):
     directory = args.fastq_dir
     directory = Path(directory).resolve()
     verb = args.verbose
-    #gets list of fastq files
+    # gets list of fastq files
     fastq_files = [f for f in listdir(directory) if isfile(join(directory, f))]
     # sets up samples with the format:
     # Sample(
@@ -98,7 +96,7 @@ def main(args):
     #           rg: str
     #           r1: str
     #           r2: str
-    #)])
+    # )])
     samples = get_samples(fastq_files)
     samples = get_readgroups(samples, fastq_files, directory, verb)
     if verb > 1:
@@ -111,7 +109,13 @@ def main(args):
         s.command = getCommand(s)
     # Constructs a dictionary with the following format:
     # "sample": ["sample name", ["rg1"...], "star command"]
-    dict = constructDict(samples)
+    sample_dict = constructDict(samples)
+    if args.output_dir:
+        directory = args.output_dir
+    else:
+        directory = directory.parent
+    print_yaml(sample_dict, directory)
+    print(f'Formed {directory}/samples.config of {len(samples)} samples.')
 
 def get_samples(fastq_files):
     # Establishes list of samples
@@ -124,22 +128,24 @@ def get_samples(fastq_files):
         # Makes a regex looking for that sample name
         name_match = re.compile(f"^{sample.name}")
         # Adds that sample to the list of samples if there isn't already one with that name initialized
-        samples if any(name_match.match(sample.name) for sample in samples) else samples.append(sample)
+        samples if any(name_match.match(s.name) for s in samples) else samples.append(sample)
     # sorts the list of samples alphabetically (had to use a lambda function because I used Sample Class)
-    samples.sort(key = lambda s : s.name)
+    samples.sort(key=lambda s: s.name)
     return samples
+
 
 # Establishes readgroups
 def get_readgroups(samples, fastq_files, fastq_dir, verb):
-    #Iterates through samples
+    # Iterates through samples
     final_samples = []
     for sample in samples:
         # each sample must have at least one R1 and R2 file to be valid
         match_r1 = re.compile(f"^{sample.name}.+?(?=R1)")
         match_r2 = re.compile(f"^{sample.name}.+?(?=R2)")
-        #Takes the first read, up to but not including the R1, as long as there is a fastq file 
+        # Takes the first read, up to but not including the R1, as long as there is a fastq file
         # for both R1 and R2 for the sample
-        readgroups = [match_r1.match(file).group(0) for file in fastq_files if match_r1.match(file) and any(match_r2.match(file) for file in fastq_files)]
+        readgroups = [match_r1.match(file).group(0) for file in fastq_files if
+                      match_r1.match(file) and any(match_r2.match(file) for file in fastq_files)]
         # Makes sure that there were some readgroups found for each sample
         if not readgroups and verb > 0:
             print(f"{sample.name} has no readgroups. {readgroups}")
@@ -168,6 +174,7 @@ def get_readgroups(samples, fastq_files, fastq_dir, verb):
             final_samples.append(sample)
     return final_samples
 
+
 def getCommand(sample):
     if len(sample.readgroups) == 1:
         return f'--readFilesIn {sample.readgroups[0].r1} {sample.readgroups[0].r2} --outSAMattrRGline {sample.readgroups[0].rg}'
@@ -181,25 +188,27 @@ def getCommand(sample):
             rgs.append(readg.rg)
         return f'--readFilesIn {",".join(r1s)} {",".join(r2s)} --outSAMattrRGline ID: {" , ID: ".join(rgs)}'
 
+
 def constructDict(samples):
     dict = {}
     for s in samples:
         rgs = [rg.rg for rg in s.readgroups]
         dict[s.name] = [s.name, rgs, s.command]
+    return dict
 
-def print_yaml():
-    data = {}
-    for sample in samples:
-        data[sample.name] = np.array(sample.readgroups)
+
+def print_yaml(sample_d, directory):
+    with open(f'{directory}/samples.config', 'w') as f:
+        data = yaml.dump(sample_d, f)
+
 
 if __name__ == "__main__":
     # Parses arugments
     # Only argument is for the directory
-    parser = argparse.ArgumentParser(description = "Create sample yaml for RNAseq mapping with STAR")
-    parser.add_argument("fastq_dir", type = dir_path, help = "Dirctory containing fastq files")
-    parser.add_argument("-o", "--output-dir", type = str, help = "Name of output directory")
-    parser.add_argument("-v", "--verbose", action = "count", default = 0, help = "Enable debug output")
+    parser = argparse.ArgumentParser(description="Create sample yaml for RNAseq mapping with STAR")
+    parser.add_argument("fastq_dir", type=dir_path, help="Dirctory containing fastq files")
+    parser.add_argument("-o", "--output-dir", type=str, help="Name of output directory")
+    parser.add_argument("-v", "--verbose", action="count", default=0, help="Enable debug output")
     args = parser.parse_args()
     main(args)
     exit(0)
-
